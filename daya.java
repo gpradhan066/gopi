7be70eed2652296af8759befa4a3ad71f7aa94a0
Gopinath pradhan <gpradhan066@gmail.com>
2023-08-15 20:02:48 +0530

    gopi second coommit

---
+++ daya.java
@@ -0,0 +1,5 @@
+public class daya{
+    public static void main(String[]args){
+System.out.println("this is the class daya");
+    }
+}
--- gopi.java
+++ gopi.java
@@ -1,5 +1,7 @@
 public class gopi{
 public static void main(String[]args){
 System.out.println("hello gopi");
+int x=10;
+int y=20;
 }
 }
